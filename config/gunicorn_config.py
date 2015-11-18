fcf22895990d793e8f3a309d2aceaf3cf2a45d0a
Irapoam Dias <irapoam@hadrons.io>
2015-11-18 15:37:49 -0200

    default config

--- config/gunicorn_config.py
+++ config/gunicorn_config.py
@@ -1,5 +1,5 @@
-command = '/home/webapps/online_agent/bin/gunicorn'
-pythonpath = '/home/webapps/online_agent/current'
+command = '/home/webapps/default/bin/gunicorn'
+pythonpath = '/home/webapps/default/current'
 bind = '127.0.0.1:8080'
 workers = 1
 user = 'root' 
