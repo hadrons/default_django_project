f177153faba92d511da1e3495c6212084c81b5ba
Irapoam Dias <irapoam@hadrons.io>
2016-05-06 14:59:40 -0300

    test deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server '111.111.111.111', user: 'root', roles: %w{app db web}
+server '54.197.23.42', user: 'root', roles: %w{app db web}
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
