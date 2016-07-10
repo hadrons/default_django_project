ddd5419e1fa9e434d8865b530ba166daa2bb233d
Irapoam Dias <irapoam.dias@gmail.com>
2016-07-10 11:11:48 -0300

    ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server '52.53.238.249', user: 'root', roles: %w{app db web}
+server '54.67.100.51', user: 'root', roles: %w{app db web}
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
